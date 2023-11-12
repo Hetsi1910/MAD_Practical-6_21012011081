76555f534c2bb4359ea4d0a39ddde64aba568d9d
Hetsi1910 <hetsipatel19@gmail.com>
2023-11-12 10:54:53 +0530

    Practical 6

---
+++ app/src/main/java/com/example/mad_practical_6_21012011081/ActivityMainBinding.kt
@@ -0,0 +1,18 @@
+package com.example.mad_practical_6_21012011081
+
+class ActivityMainBinding {
+
+    val btnNext: Any
+    val btnPrevious: Any
+    val btnShuffle: Any
+    val toolbar: Toolbar?
+    val root: Any
+    val btnStop: Any
+    val btnPlay: Any
+
+    companion object {
+        fun inflate(layoutInflater: LayoutInflater): ActivityMainBinding {
+
+        }
+    }
+}
